9bdc22ab6ec0da6c44658381cb2b4958baa60778
keshava Banda <keshava Banda@LAPTOP-KIRAP7UP>
2020-09-14 16:33:51 +0530

    First Commit

---
+++ DemoGit/src/com/hi/in/Hello.java
@@ -0,0 +1,15 @@
+package com.hi.in;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello Git ");
+		System.out.println("This is My First Project in GitHub");
+		System.out.println("Hi Everyone Github Users");
+
+	}
+
+}
